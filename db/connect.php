4ad4239d1ad9adb8f0a45019c653f1b74dee131a
amsteri <medvedevksm@gmail.com>
2017-05-08 12:21:53 +0300

    ura

--- db/connect.php
+++ db/connect.php
@@ -1,8 +1,9 @@
 <?php
+	define('hostname', '127.0.0.1');
 	define('login', 'root');
 	define('password', '1111');
 	try {
-		$handler = new PDO('mysql:host=127.0.0.1;dbname=medvedev', login, password);
+		$handler = new PDO('mysql:host='.hostname.';dbname=medvedev', login, password);
 		$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (PDOException $e) {
 		echo "ERROR: " . $e->getMessage();
